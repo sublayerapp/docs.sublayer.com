a53a37838148d4f007bdbcced0e4ecba5f714ee4
Scott Werner <stwerner@vt.edu>
2024-04-05 16:29:04 -0400

    Update iframe to dynamically resize

--- src/components/Iframe.jsx
+++ src/components/Iframe.jsx
@@ -1,6 +1,25 @@
+'use client';
+import React, { useEffect } from 'react';
+
 export function Iframe({ path }) {
   const src = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://blueprints.sublayer.com';
+
+  // Add an event listener to resize the iframe
+  useEffect(() => {
+    function resizeIframe(event) {
+      console.log(event);
+      const iframe = document.querySelector('iframe');
+      iframe.style.height = event.data.height + 'px';
+    }
+
+    window.addEventListener('message', resizeIframe);
+
+    return () => {
+      window.removeEventListener('message', resizeIframe);
+    }
+  })
+
   return (
-    <iframe src={`${src}/${path}`} width="100%" height="400px" title="Rails Content"></iframe>
+    <iframe src={`${src}/${path}`} width="100%" title="Rails Content"></iframe>
   )
 }
